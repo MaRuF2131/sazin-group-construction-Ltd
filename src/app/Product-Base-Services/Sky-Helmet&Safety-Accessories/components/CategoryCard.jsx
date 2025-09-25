a8106e3627cf6945235747e6c4df98c1f2f00fd9
Jannatul <limajannat1675@gmail.com>
2025-09-26 01:56:57 +0600

    added more card to helmets section

--- src/app/Product-Base-Services/Sky-Helmet&Safety-Accessories/components/CategoryCard.jsx
+++ src/app/Product-Base-Services/Sky-Helmet&Safety-Accessories/components/CategoryCard.jsx
@@ -32,19 +32,78 @@ const subCategories = [
         id:4
     },
       {
-      title: "Half Face",
+      title: "Welding Helmets & Gloves",
       image:
         "/halfface.jpg",
-        path:'/Product-Base-Services/Sky-Helmet&Safety-Accessories/Half-Face',
-        id:4
+        path:'//Product-Base-Services/Safety-Accessories/Welding',
+        id:5
     },
    
     {
-      title: "Half Face",
+      title: "Fall Protection Harness",
       image:
         "/halfface.jpg",
-        path:'/Product-Base-Services/Sky-Helmet&Safety-Accessories/Half-Face',
-        id:4
+        path:'/Product-Base-Services/Safety-Accessories/Harness',
+        id:6
+    },
+    {
+      title: "Coveralls / Suits",
+      image:
+        "/fullface.jpg",
+
+        path:'/Product-Base-Services/Safety-Accessories/Coverall',
+        id:7
+    },
+    {
+      title: "Respirators / Masks",
+      image:
+        "/modulerface.jpg",
+        path:'/Product-Base-Services/Safety-Accessories/Masks',
+        id:8
+    },
+    {
+      title: "High-Visibility Safety Vests",
+      image:
+        "/openface.jpg",
+        path:'/Product-Base-Services/Safety-Accessories/Vests',
+        id:9
+    },
+    {
+      title: "Safety Shoes / Gumboots",
+      image:
+        "/halfface.jpg",
+        path:'/Product-Base-Services/Safety-Accessories/Shoes',
+        id:10
+    },
+      {
+      title: "Safety Gloves",
+      image:
+        "/halfface.jpg",
+        path:'/Product-Base-Services/Safety-Accessories/Gloves',
+        id:11
+    },
+   
+    {
+      title: "Ear Plugs / Ear Muffs",
+      image:
+        "/halfface.jpg",
+        path:'/Product-Base-Services/Safety-Accessories/Hearing-Protection',
+        id:12
+    },
+      {
+      title: "Safety Goggles / Face Shields",
+      image:
+        "/halfface.jpg",
+        path:'/Product-Base-Services/Safety-Accessories/Goggles',
+        id:13
+    },
+   
+    {
+      title: "Safety Helmets (Hard Hats)",
+      image:
+        "/halfface.jpg",
+        path:'/Product-Base-Services/Safety-Accessories/Helmets',
+        id:14
     },
 
   ];
